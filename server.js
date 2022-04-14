97e1a99a3978c9949fe34ccc58296504f6109e71
apoorvpandey0 <apoorvpandey0@gmail.com>
2022-04-14 20:44:29 +0530

    added ejs templating engine

--- server.js
+++ server.js
@@ -2,6 +2,10 @@ console.log("Hello from server.js!");
 const express = require("express");
 const app = express();
 
+// Like Django templating engine we have ejs here
+// Setting templating engine for app
+app.set("view engine", "ejs");
+
 app.get("/", (req, res) => {
     console.log("iNSIDE GET REQUEST");
 
@@ -9,12 +13,14 @@ app.get("/", (req, res) => {
     // res.sendStatus(500).send("Hello World! This is an error");
 
     // Send error codes with JSON msg for html display
-    res.sendStatus(500).json({ 'error': 'Hello World! This is an error' });
+    // res.sendStatus(500).json({ 'error': 'Hello World! This is an error' });
 
-    res.send("Hello from server.js!");
+    // res.send("Hello from server.js!");
 
     // will have has no effect since one send is already done
     // res.send("Hello from server.js! 2");
+
+    res.render("index", { 'text': 'World' });
 })
 
 
